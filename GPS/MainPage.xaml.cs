70ee0608a6e0d2bda358d9b51fbb1556ce83b895
declan begley <declanbegley@outlook.com>
2020-05-18 15:26:12 +0100

    Add project files.

---
+++ GPS/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace GPS
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var location = await Geolocation.GetLastKnownLocationAsync();
+                if (location == null) 
+                {
+                    location = await Geolocation.GetLocationAsync(new GeolocationRequest
+                    {
+                        DesiredAccuracy = GeolocationAccuracy.Medium,
+                        Timeout = TimeSpan.FromSeconds(30)
+                    });                 
+                                   
+                }
+                if (location == null)
+                    LabelLocation.Text  = "NO GPS";
+                else
+                    LabelLocation.Text  = $"{location.Latitude} {location.Longitude} ";
+
+            }
+            catch (Exception ex )
+            {
+
+                Debug.WriteLine($"something is work {ex.Message}");
+            }
+        }
+    }
+}
